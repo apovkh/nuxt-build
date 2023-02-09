351c3d73a71e13e35b4275472eb8865c39e5fd41
Anatolii Povkh <apovkh.dev@gmail.com>
2023-02-09 21:51:22 +0200

    feat: add vuetify & mdi icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,17 @@ export default defineNuxtConfig({
 	srcDir: './src',
 	modules: [
 		'nuxt-windicss'
-	]
+	],
+	css: [
+		'vuetify/lib/styles/main.sass',
+		'@mdi/font/css/materialdesignicons.min.css'
+	],
+	build: {
+		transpile: ['vuetify']
+	},
+	vite: {
+		define: {
+			'process.env.DEBUG': false
+		}
+	}
 })
